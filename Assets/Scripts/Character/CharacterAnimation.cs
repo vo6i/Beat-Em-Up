4fb3dbbb96f1f76e51c4b15b37c53007bcc28777
Ustym Ukhman <ustym.ukhman@gmail.com>
2020-11-15 04:26:52 +0200

    Add player walk animation

---
+++ Assets/Scripts/Character/CharacterAnimation.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    public void Walk(bool move)
+    {
+        animator.SetBool(AnimationTags.MOVEMENT, move);
+    }
+
+    public void Punch1()
+    {
+        animator.SetTrigger(AnimationTags.PUNCH1);
+    }
+
+    public void Punch2()
+    {
+        animator.SetTrigger(AnimationTags.PUNCH2);
+    }
+
+    public void Punch3()
+    {
+        animator.SetTrigger(AnimationTags.PUNCH3);
+    }
+
+    public void Kick1()
+    {
+        animator.SetTrigger(AnimationTags.KICK1);
+    }
+
+    public void Kick2()
+    {
+        animator.SetTrigger(AnimationTags.KICK2);
+    }
+}
---
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // private float horizontalRotation = -90.0f;
+    // private float verticalRotation = -180.0f;
+
+    public float horizontalSpeed = 2.0f;
+    public float verticalSpeed = 1.5f;
+
+    private CharacterAnimation animation;
+    private Rigidbody body;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        animation = GetComponentInChildren<CharacterAnimation>();
+    }
+
+    void Update()
+    {
+        UpdatePlayerRotation();
+        UpdatePlayerAnimation();
+    }
+
+    private void UpdatePlayerRotation()
+    {
+        if (Input.GetAxisRaw(Axis.HORIZONTAL) == 1)
+        {
+            transform.rotation = Quaternion.Euler(0.0f, -90.0f, 0.0f);
+        }
+        else if (Input.GetAxisRaw(Axis.HORIZONTAL) == -1)
+        {
+            transform.rotation = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+        }
+
+
+        if (Input.GetAxisRaw(Axis.VERTICAL) == 1)
+        {
+            transform.rotation = Quaternion.Euler(0.0f, -180.0f, 0.0f);
+        }
+        else if (Input.GetAxisRaw(Axis.VERTICAL) == -1)
+        {
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+        }
+    }
+
+    private void UpdatePlayerAnimation()
+    {
+        if (Input.GetAxisRaw(Axis.HORIZONTAL) != 0 || Input.GetAxisRaw(Axis.VERTICAL) != 0)
+        {
+            animation.Walk(true);
+        }
+        else
+        {
+            animation.Walk(false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        body.velocity = new Vector3(
+            Input.GetAxisRaw(Axis.HORIZONTAL) * -horizontalSpeed,
+            body.velocity.y,
+            Input.GetAxisRaw(Axis.VERTICAL) * -verticalSpeed
+        );
+    }
+}
