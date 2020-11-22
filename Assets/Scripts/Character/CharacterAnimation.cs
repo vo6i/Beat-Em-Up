61e98e3534fc0d64b5af0e3f38b2eb8decb00454
Ustym Ukhman <ustym.ukhman@gmail.com>
2020-11-22 14:18:52 +0200

    EnemyManager script added

--- Assets/Scripts/Character/CharacterAnimation.cs
+++ Assets/Scripts/Character/CharacterAnimation.cs
@@ -10,10 +10,22 @@ public class CharacterAnimation : MonoBehaviour
     {
         animator = GetComponent<Animator>();
     }
-    
+
+    public void Idle()
+    {
+        animator.Play(AnimationTags.IDLE);
+    }
+
     public void Walk(bool move)
     {
         animator.SetBool(AnimationTags.MOVEMENT, move);
+    }
+
+    public void EnemyAttack(int attack)
+    {
+             if (attack == 0) animator.SetTrigger(AnimationTags.ATTACK1);
+        else if (attack == 1) animator.SetTrigger(AnimationTags.ATTACK2);
+        else if (attack == 2) animator.SetTrigger(AnimationTags.ATTACK3);
     }
 
     public void Punch1()
@@ -39,5 +51,25 @@ public class CharacterAnimation : MonoBehaviour
     public void Kick2()
     {
         animator.SetTrigger(AnimationTags.KICK2);
+    }
+
+    public void Hit()
+    {
+        animator.SetTrigger(AnimationTags.HIT);
+    }
+
+    public void KnockDown()
+    {
+        animator.SetTrigger(AnimationTags.KNOCK_DOWN);
+    }
+
+    public void StandUp()
+    {
+        animator.SetTrigger(AnimationTags.STAND_UP);
+    }
+
+    public void Death()
+    {
+        animator.SetTrigger(AnimationTags.DEATH);
     }
 }
---
+++ Assets/Scripts/Character/EnemyManager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyManager : MonoBehaviour
+{
+    private new CharacterAnimation animation;
+    private bool followPlayer, attackPlayer;
+
+    public float attackDistance = 1.25f;
+    private float defaultAttack = 0.75f;
+    private float chaseDistance = 0.25f;
+
+    private Transform playerTarget;
+    public float speed = 2.0f;
+
+    private float attackTime;
+    private Rigidbody body;
+
+    void Awake()
+    {
+        playerTarget = GameObject.FindWithTag(ObjectTags.PLAYER).transform;
+        animation = GetComponentInChildren<CharacterAnimation>();
+        body = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        attackTime = defaultAttack;
+        followPlayer = true;
+    }
+
+    void Update()
+    {
+        if (attackPlayer)
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        float followDistance = attackDistance + chaseDistance;
+        attackTime += Time.deltaTime;
+
+        if (attackTime > defaultAttack)
+        {
+            animation.EnemyAttack(Random.Range(0, 3));
+            attackTime = 0.0f;
+        }
+
+        if (DistanceToPlayer() > followDistance)
+        {
+            attackPlayer = false;
+            followPlayer = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (followPlayer)
+        {
+            FollowTarget();
+        }
+    }
+
+    void FollowTarget()
+    {
+        if (DistanceToPlayer() > attackDistance)
+        {
+            body.velocity = transform.forward * speed;
+            if (!attackPlayer) animation.Walk(true);
+            transform.LookAt(playerTarget);
+        }
+        else
+        {
+            body.velocity = Vector3.zero;
+            animation.Walk(false);
+
+            followPlayer = false;
+            attackPlayer = true;
+        }
+    }
+
+    private float DistanceToPlayer()
+    {
+        return Vector3.Distance(transform.position, playerTarget.position);
+    }
+}
